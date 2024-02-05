fee5527396df8624ca17fe431e375e23fc3e83d8
Eric Costa <frodoeric@hotmail.com>
2024-02-05 15:05:23 +0000

    added entity and valueObjects

---
+++ UtmBuilder.Core/Utm.cs
@@ -0,0 +1,10 @@
+﻿using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core
+{
+    public class Utm(Url url, Campaign campaign)
+    {
+        public Url Url { get; set; } = url;
+        public Campaign Campaign { get; set; } = campaign;
+    }
+}
---
+++ UtmBuilder.Core/ValueObjects/Url.cs
@@ -0,0 +1,14 @@
+﻿namespace UtmBuilder.Core.ValueObjects
+{
+    /// <summary>
+    /// Create a new URL
+    /// </summary>
+    /// <param name="address">Address of URL (Website link)</param>
+    public class Url(string address) : ValueObject
+    {
+        /// <summary>
+        /// Address of URL (Website link)
+        /// </summary>
+        public string Address { get; } = address;
+    }
+}
