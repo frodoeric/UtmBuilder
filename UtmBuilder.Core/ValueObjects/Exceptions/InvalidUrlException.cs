c6660087738597a05cc18986972ec5490780252d
Eric Costa <frodoeric@hotmail.com>
2024-02-08 14:08:55 +0000

    added verification into the exception class

--- UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
+++ UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
@@ -1,6 +1,26 @@
-﻿namespace UtmBuilder.Core.ValueObjects.Exceptions
+﻿
+
+using System.Text.RegularExpressions;
+
+namespace UtmBuilder.Core.ValueObjects.Exceptions
 {
-    public class InvalidUrlException(string message = "Invalid URL") : Exception(message)
+    public partial class InvalidUrlException(string message) : Exception(message)
     {
+        private const string DefaultErrorMessage = "Invalid URL";
+        private const string UrlRegex = @"^(https?|ftp)://[^\s/$.?#].[^\s]*$";
+        [GeneratedRegex(UrlRegex)]
+        private static partial Regex Urlregex();
+
+        public static void ThrowIfInvalidUrl(string url, string message = DefaultErrorMessage)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+                throw new InvalidUrlException(message);
+
+            if (!Urlregex().IsMatch(url))
+                throw new InvalidUrlException(message);
+        }
+        
     }
+
+    
 }
--- UtmBuilder.Core/ValueObjects/Url.cs
+++ UtmBuilder.Core/ValueObjects/Url.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.RegularExpressions;
-using UtmBuilder.Core.ValueObjects.Exceptions;
+﻿using UtmBuilder.Core.ValueObjects.Exceptions;
 
 namespace UtmBuilder.Core.ValueObjects
 {
@@ -9,22 +8,16 @@ namespace UtmBuilder.Core.ValueObjects
     /// <param name="address">Address of URL (Website link)</param>
     public partial class Url : ValueObject
     {
-        private const string UrlRegex = @"^(https?|ftp)://[^\s/$.?#].[^\s]*$";
 
         public Url(string address)
         {
             Address = address;
-            if (!Urlregex().IsMatch(address))            
-                throw new InvalidUrlException();
-            
+            InvalidUrlException.ThrowIfInvalidUrl(address, "Invalid URL");            
         }
 
         /// <summary>
         /// Address of URL (Website link)
         /// </summary>
         public string Address { get; }
-
-        [GeneratedRegex(UrlRegex)]
-        private static partial Regex Urlregex();
     }
 }
